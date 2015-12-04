627d3dd2539d9ca7e599a773b0ead561946f5ddb
Andreas Ek <andreas@andreasek.se>
2015-12-04 11:06:06 +0100

    GA on the right place

--- web/app/themes/aekab/views/parts/header.php
+++ web/app/themes/aekab/views/parts/header.php
@@ -1,3 +1,14 @@
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60162402-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
   <div class="w-section w-clearfix header">
     <div class="w-hidden-small w-hidden-tiny headersocialright">
       <a href="http://www.linkedin.com/aekab" class="w-hidden-medium w-hidden-small w-hidden-tiny w-inline-block btnsocialheader"><img alt="LinkedIn" width="36" src="{{ ekandreas\Assets\asset_path('images/icon-in-header.png') }}">
