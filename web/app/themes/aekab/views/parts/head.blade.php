70b03fa1949dbe3924ea1c19d6c9315c753ec0f9
Andreas Ek <andreas@andreasek.se>
2016-05-08 21:33:01 +0200

    Change favicons again

--- web/app/themes/aekab/views/parts/head.blade.php
+++ web/app/themes/aekab/views/parts/head.blade.php
@@ -14,23 +14,22 @@
   </script>
   <script type="text/javascript" src="{{ get_stylesheet_directory_uri() . '/assets/scripts/modernizr.js' }}"></script>
 
-  <link rel="apple-touch-icon" sizes="57x57" href="{{ ekandreas\Assets\asset_path('images/apple-touch-icon-57x57.png') }}">
-  <link rel="apple-touch-icon" sizes="60x60" href="{{ ekandreas\Assets\asset_path('images/apple-touch-icon-60x60.png') }}">
-  <link rel="apple-touch-icon" sizes="72x72" href="{{ ekandreas\Assets\asset_path('images/apple-touch-icon-72x72.png') }}">
-  <link rel="apple-touch-icon" sizes="76x76" href="{{ ekandreas\Assets\asset_path('images/apple-touch-icon-76x76.png') }}">
-  <link rel="apple-touch-icon" sizes="114x114" href="{{ ekandreas\Assets\asset_path('images/apple-touch-icon-114x114.png') }}">
-  <link rel="apple-touch-icon" sizes="120x120" href="{{ ekandreas\Assets\asset_path('images/apple-touch-icon-120x120.png') }}">
-  <link rel="apple-touch-icon" sizes="144x144" href="{{ ekandreas\Assets\asset_path('images/apple-touch-icon-144x144.png') }}">
-  <link rel="apple-touch-icon" sizes="152x152" href="{{ ekandreas\Assets\asset_path('images/apple-touch-icon-152x152.png') }}">
-  <link rel="apple-touch-icon" sizes="180x180" href="{{ ekandreas\Assets\asset_path('images/apple-touch-icon-180x180.png') }}">
-  <link rel="icon" type="image/png" href="{{ ekandreas\Assets\asset_path('images/favicon-32x32.png') }}" sizes="32x32">
-  <link rel="icon" type="image/png" href="{{ ekandreas\Assets\asset_path('images/android-chrome-192x192.png') }}" sizes="192x192">
-  <link rel="icon" type="image/png" href="{{ ekandreas\Assets\asset_path('images/favicon-96x96.png') }}" sizes="96x96">
-  <link rel="icon" type="image/png" href="{{ ekandreas\Assets\asset_path('images/favicon-16x16.png') }}" sizes="16x16">
-  <link rel="manifest" href="{{ ekandreas\Assets\asset_path('images/manifest.json') }}">
-  <link rel="mask-icon" href="{{ ekandreas\Assets\asset_path('images/safari-pinned-tab.svg') }}" color="#5bbad5">
-  <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="msapplication-TileImage" content="{{ ekandreas\Assets\asset_path('images/mstile-144x144.png') }}">
+  <link rel="apple-touch-icon" sizes="57x57" href="{{ ekandreas\Assets\asset_path('images/favicons/apple-icon-57x57.png') }}">
+  <link rel="apple-touch-icon" sizes="60x60" href="{{ ekandreas\Assets\asset_path('images/favicons/apple-icon-60x60.png') }}">
+  <link rel="apple-touch-icon" sizes="72x72" href="{{ ekandreas\Assets\asset_path('images/favicons/apple-icon-72x72.png') }}">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ ekandreas\Assets\asset_path('images/favicons/apple-icon-76x76.png') }}">
+  <link rel="apple-touch-icon" sizes="114x114" href="{{ ekandreas\Assets\asset_path('images/favicons/apple-icon-114x114.png') }}">
+  <link rel="apple-touch-icon" sizes="120x120" href="{{ ekandreas\Assets\asset_path('images/favicons/apple-icon-120x120.png') }}">
+  <link rel="apple-touch-icon" sizes="144x144" href="{{ ekandreas\Assets\asset_path('images/favicons/apple-icon-144x144.png') }}">
+  <link rel="apple-touch-icon" sizes="152x152" href="{{ ekandreas\Assets\asset_path('images/favicons/apple-icon-152x152.png') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ ekandreas\Assets\asset_path('images/favicons/apple-icon-180x180.png') }}">
+  <link rel="icon" type="image/png" sizes="192x192"  href="{{ ekandreas\Assets\asset_path('images/favicons/android-icon-192x192.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ ekandreas\Assets\asset_path('images/favicons/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="96x96" href="{{ ekandreas\Assets\asset_path('images/favicons/favicon-96x96.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ ekandreas\Assets\asset_path('images/favicons/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ ekandreas\Assets\asset_path('images/favicons/manifest.json') }}">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="{{ ekandreas\Assets\asset_path('images/favicons/ms-icon-144x144.png') }}">
   <meta name="theme-color" content="#ffffff">
 
   @if( has_post_thumbnail(get_the_ID()) )
